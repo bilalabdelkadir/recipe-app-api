f73f94492f28bec43599812cc861463b4e67ce58
bilalabdelkadir <bilalbinabdelkadir@gmail.com>
2023-10-07 15:59:05 +0300

    feat: added testing setup

---
+++ app/app/calc.py
@@ -0,0 +1,15 @@
+"""
+Calculator module
+"""
+
+def add(x,y):
+    """
+    adds x and y
+    """
+    return x + y
+
+def generate_username(name, age):
+    """
+    takes name and returns the name + age as username
+    """
+    return name + str(age)
